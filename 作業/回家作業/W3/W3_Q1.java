d7cc71ab577a6b796574cd1d59e89ab544a26449
NTX8205 <70540067+NTX8205@users.noreply.github.com>
2022-03-11 13:21:03 +0800

    init

---
+++ 作業/回家作業/W3/W3_Q1.java
@@ -0,0 +1,17 @@
+package 作業.回家作業.W3;
+
+
+/**
+ * 題目:排序次序
+ * 班級:資管二B
+ * 姓名:許哲睿
+ * 學號:410928050
+ * 日期:2022.03.09
+ */
+
+public class W3_Q1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
