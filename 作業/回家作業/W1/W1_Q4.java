4ef589223283ddea04c9afb2897c542e5260b2d4
NTX8205 <70540067+NTX8205@users.noreply.github.com>
2022-03-16 23:34:02 +0800

    Create W1_Q4.java

---
+++ 作業/回家作業/W1/W1_Q4.java
@@ -0,0 +1,37 @@
+package 作業.回家作業.W1;
+
+/**
+ * 題目:Q4: 計算兩個正整數的最大公因數&最小公倍數
+ * 班級:資管二B
+ * 姓名:許哲睿
+ * 學號:410928050
+ * 日期:2022.03.16
+ */
+
+import java.util.*;
+public class W1_Q4 {
+    
+
+    public static void main(String[] args) {
+
+        System.out.println("計算兩個正整數的最大公因數&最小公倍數");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("輸入2數:");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        sc.close();
+        int a=x,b=y;
+
+        int gcd = 0;
+        while (a % b != 0) {
+            gcd = b;
+            b = a % b;
+            a = gcd;
+        }
+        System.out.println(x + "和" + y + "的最大公因數:" + b);
+        System.out.println(x + "和" + y + "的最小公倍數:" + x*y/b);
+
+    }
+}
