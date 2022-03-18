dbf3e22624de91a900d61be89e1768922acc03ae
NTX8205 <70540067+NTX8205@users.noreply.github.com>
2022-03-18 15:55:20 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,35 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main{
 
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
 
         ArrayList<Integer> test = new ArrayList<Integer>();
+        
+        int n=sc.nextInt();
+        int count=n;
 
+        for(int i=0;i<5;i++){
+            test.add(i,i+1);
+        }
+        
 
+        while(count!=1){
+            if(count!=2){
+                int tmp=(int)test.get(0);
+                int tmp1=(int)test.get(1);
+                
+
+                for(int i=0;i<n-3;i++){
+                    test.set(i, test.get(2+i));
+                } 
+            }
+            else{
+                test.remove(0);
+            }
+        }
     }
 }
