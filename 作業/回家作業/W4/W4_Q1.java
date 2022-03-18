6d6446b3a31478fe7a7facb02d722812374c2d14
NTX8205 <70540067+NTX8205@users.noreply.github.com>
2022-03-18 15:55:22 +0800

    Update W4_Q1.java

--- 作業/回家作業/W4/W4_Q1.java
+++ 作業/回家作業/W4/W4_Q1.java
@@ -1,5 +1,17 @@
 package 作業.回家作業.W4;
 
+
+import java.util.*;
 public class W4_Q1 {
     
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if(n>=5){
+            
+        }
+    }
 }
