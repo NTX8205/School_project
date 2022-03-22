8352087e7a326369cfe9bcdedf27d6ca1c69303e
NTX8205 <70540067+NTX8205@users.noreply.github.com>
2022-03-22 22:12:24 +0800

    Update W4_Q1.java

--- 作業/回家作業/W4/W4_Q1.java
+++ 作業/回家作業/W4/W4_Q1.java
@@ -1,5 +1,14 @@
 package 作業.回家作業.W4;
 
+/**
+ * 題目:三號出局
+ * 班級:資管二B
+ * 姓名:許哲睿
+ * 學號:410928050
+ * 日期:2022.03.22
+ */
+
+
 
 import java.util.*;
 public class W4_Q1 {
@@ -8,10 +17,36 @@ public class W4_Q1 {
         
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        ArrayList<Integer> num = new ArrayList<Integer>();
 
+        System.out.println("三號出局");
+        System.out.print("輸入人數: ");
+        int n = sc.nextInt();
         if(n>=5){
-            
+
+            for(int i = 0;i<n ; i++){
+                num.add(i+1);
+            }
+            int count = 1;
+            while(num.size()>1){
+                for(int i = 0; i<num.size();i++){
+                    if(count==3){
+                        num.remove(i);
+                        count=1;
+                        i--;
+                    }
+                    else{
+                        count++;
+                    }
+                    if(i==num.indexOf(n)){
+                        break;
+                    }
+                }
+            }
+            System.out.println("最後一位編號:"+num.get(0));
+        }
+        else{
+            System.out.println("人數少於5人，無法計算");
         }
     }
 }
