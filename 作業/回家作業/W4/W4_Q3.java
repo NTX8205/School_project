5ea05be8e088187773ed22d4108378ff91b6b4cb
NTX8205 <70540067+NTX8205@users.noreply.github.com>
2022-03-23 00:17:14 +0800

    Update W4_Q3.java

--- 作業/回家作業/W4/W4_Q3.java
+++ 作業/回家作業/W4/W4_Q3.java
@@ -1,5 +1,37 @@
 package 作業.回家作業.W4;
 
+/**
+ * 題目:圓括號對應Parenthesis Matching
+ * 班級:資管二B
+ * 姓名:許哲睿
+ * 學號:410928050
+ * 日期:2022.03.22
+ */
+
+import java.util.*;
 public class W4_Q3 {
     
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        String s = sc.next();
+        ArrayList<String> str = new ArrayList<String>();
+        ArrayList<Integer> positon = new ArrayList<Integer>();
+
+        for(int i =0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c==40){
+                str.add("(");
+                positon.add(i);
+            }
+            else if(c==41){
+                str.add(")");
+                positon.add(i);
+            }
+        }
+
+        System.out.println(str);
+        System.out.println(positon);
+
+    }
 }
