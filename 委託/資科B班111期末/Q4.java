67d820e338c34676f72f8f68bb0d89cc8180c054
NTX8205 <ad0124578963@gmail.com>
2022-06-09 15:22:55 +0800

    Q4 完成

--- 委託/資科B班111期末/Q4.java
+++ 委託/資科B班111期末/Q4.java
@@ -10,6 +10,23 @@ package 委託.資科B班111期末;
  * 60 20 90 100 50 40 70 10 80 30
  */
 
+import java.util.*;
 public class Q4 {
-    
+    public static void main(String[] args) {
+        long startTime = System.nanoTime(); //起始時間
+        int[] array = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        System.out.println("隨機排列前的陣列 :" + Arrays.toString(array));
+        //隨機轉換陣列裡的數字位置
+        for (int i = 0; i < array.length; i++) {
+            int random = (int) (Math.random() * 10);//選擇要交換的位置
+            //目前循序到的數字和指定位置的數字進行交換
+            int temp = array[i];
+            array[i] = array[random];
+            array[random] = temp;
+        }
+        System.out.println("隨機排列後的陣列 :" + Arrays.toString(array));
+        long endTime = System.nanoTime();//結束時間
+        double duringTime = (double) (endTime - startTime) / 1000000000; //換算成秒
+        System.out.println("此程式執行時間 : "+ duringTime +"秒");
+    }
 }
