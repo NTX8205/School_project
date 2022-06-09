9a16bb6bf41cf9e4d4e4f3572931a0a896e51928
NTX8205 <ad0124578963@gmail.com>
2022-06-09 09:43:28 +0800

    Q5 完成

--- 委託/資科B班111期末/Q5.java
+++ 委託/資科B班111期末/Q5.java
@@ -39,9 +39,58 @@ package 委託.資科B班111期末;
  * }
  * 
  * 問題 1: 上面執行結果為何?
+ * A : Animal talk
+ *     Dog talk
+ *     Cat talk
+ *     Dog talk
  * 問題 2: Animal d = new Dog(); 這段程式碼是什麼意思，請說明?
+ * A : 新增一個Animal的物件d，其型態為Dog。
  */
 
 public class Q5 {
-    
+    //因為沒有move的方法用，因此將其改為talk。
+    public static void talk_Animal(Animal obj) {
+        obj.talk();
+    }
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        Dog b = new Dog();
+        Cat c = new Cat();
+        Animal d = new Dog();
+        talk_Animal (a);
+        talk_Animal (b);
+        talk_Animal (c);
+        talk_Animal (d);
+        
+    }
+}
+
+class Animal {
+    public Animal (){
+
+    }
+
+    public void talk() {
+        System.out.println("Animal talk");
+    }
+}
+
+class Dog extends Animal {
+    public Dog (){
+
+    }
+
+    public void talk() {
+        System.out.println("Dog talk");
+    }
+}
+
+class Cat extends Animal {
+    public Cat (){
+
+    }
+
+    public void talk() {
+        System.out.println("Cat talk");
+    }
 }
