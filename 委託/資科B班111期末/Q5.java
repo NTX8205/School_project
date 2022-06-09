745258e6bcf6c3bdffe6646756bfb3873f3fe3c6
NTX8205 <ad0124578963@gmail.com>
2022-06-09 09:15:26 +0800

    期末

---
+++ 委託/資科B班111期末/Q5.java
@@ -0,0 +1,47 @@
+package 委託.資科B班111期末;
+
+/*
+ * 題目 5: 物件導向 (20%)
+ * 說明: 有四個類別(class)，Animal, Dog, Cat, main，請回答下列問題:
+ * 
+ * class Animal{
+ *      Animal(){}
+ *      void talk(){
+ *          System.out.println(“Animal talk”);
+ *      }
+ * } 
+ * class Dog extends Animal{
+ *      Dog(){}
+ *      void talk(){
+ *          System.out.println(“Dog talk”);
+ *      }
+ * }
+ * class Cat extends Animal{
+ *      Cat(){}
+ *      void talk(){ 
+ *          System.out.println(“Cat talk”);
+ *      }
+ * }
+ * public class main{
+ *      static void move_Animal(Animal obj){
+ *          obj.move();
+ *      }
+ *      public static void main(){
+ *          Animal a = new Animal();
+ *          Dog b = new Dog();
+ *          Cat c = new Cat();
+ *          Animal d = new Dog();
+ *          move_Animal (a);
+ *          move_Animal (b);
+ *          move_Animal (c);
+ *          move_Animal (d);
+ *      }
+ * }
+ * 
+ * 問題 1: 上面執行結果為何?
+ * 問題 2: Animal d = new Dog(); 這段程式碼是什麼意思，請說明?
+ */
+
+public class Q5 {
+    
+}
